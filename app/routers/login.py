ad00724948b4eef22f39421eee3a07ce9b01851e
0xDkXy <bo001015@outlook.com>
2022-01-01 02:39:01 +0800

    更新了登陆验证

--- app/routers/login.py
+++ app/routers/login.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter,Depends
 from pydantic import BaseModel
 from pydantic.errors import UrlSchemeError
+from hashlib import sha256
+from ..DataBase.loginDB import loginQuery 
 
 class loginModel(BaseModel):
     id:str
     passwd:str
-
 
 router=APIRouter(
     prefix='/login',
@@ -15,7 +16,17 @@ router=APIRouter(
 
 @router.post('/')
 async def loginRoot(user:loginModel):
-    return {
-        'id':user.id,
-        'token':'xxxxxxx',
-    }
+    encry=sha256()
+    pwd=user.passwd.encode()
+    encry.update(pwd)
+    pwdSha256=encry.hexdigest()
+    del encry
+    if loginQuery(user=user.id,passwd=pwdSha256):return {
+            'id':user.id,
+            'token':'',
+            'sha256':pwdSha256,
+        }
+    else:
+        return {'msg':'wa',}
+
+    
