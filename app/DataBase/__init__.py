f82158bdc870d5bb8f60f17d15863e8ec94ca743
0xDkXy <bo001015@outlook.com>
2022-01-01 17:22:29 +0800

    初始化数据库模块

--- app/DataBase/__init__.py
+++ app/DataBase/__init__.py
@@ -1,22 +1,22 @@
 import sqlite3
 from loguru import logger
 from hashlib import sha256
-from ..tools.Logger import logger 
+from ..tools.Logger import logger
 import traceback
 
 try:
-    conn=sqlite3.connect('./data/user.db')
+    conn = sqlite3.connect('./data/user.db')
     logger.info('database conneted!')
-    cur=conn.cursor()
+    cur = conn.cursor()
     cur.execute('create table if not exists UserPasswd \
         (ID VARCHAR PRIMARY KEY     NOT NULL,\
         PASSWD VARCHAR NOT NULL\
         );\
     ')
-    encry=sha256()
+    encry = sha256()
     encry.update(b'admin')
-    hashAdminPwd=encry.hexdigest()
-    Qstr='\
+    hashAdminPwd = encry.hexdigest()
+    Qstr = '\
     INSERT INTO UserPasswd (ID,PASSWD)\
     VALUES ("admin","{}")'.format(hashAdminPwd)
     cur.execute(Qstr)
@@ -24,7 +24,7 @@ try:
 except:
     logger.warning('database connect failed')
     logger.warning(traceback.format_exc())
-    
+
 
 # cur.execute('''
 #     create table ''')
