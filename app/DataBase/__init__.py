082c1f5ccb0ddba1c4879abfe78b7cf7431e721c
0xDkXy <bo001015@outlook.com>
2022-01-02 16:07:03 +0800

    update

--- app/DataBase/__init__.py
+++ app/DataBase/__init__.py
@@ -2,7 +2,11 @@ import sqlite3
 from loguru import logger
 from hashlib import sha256
 from ..tools.Logger import logger
-import traceback
+import traceback,os
+
+# 创建数据存放目录
+if not os.path.exists(r'./data'):
+    os.mkdir(r'data')
 
 try:
     conn = sqlite3.connect('./data/user.db')
@@ -13,17 +17,36 @@ try:
         PASSWD VARCHAR NOT NULL\
         );\
     ')
+    conn.commit()
     encry = sha256()
     encry.update(b'admin')
     hashAdminPwd = encry.hexdigest()
-    Qstr = '\
-    INSERT INTO UserPasswd (ID,PASSWD)\
-    VALUES ("admin","{}")'.format(hashAdminPwd)
-    cur.execute(Qstr)
+    Qstr='select ID,PASSWD FROM UserPasswd\
+        WHERE ID="{}" and PASSWD="{}"'.format('admin',hashAdminPwd)
+    val=cur.execute(Qstr)
     conn.commit()
+    if not val:
+        Qstr = '\
+        INSERT INTO UserPasswd (ID,PASSWD)\
+        VALUES ("admin","{}")'.format(hashAdminPwd)
+        cur.execute(Qstr)
+        conn.commit()
+        logger.info('创建管理员账户成功！')
+    else:
+        logger.info('已有管理员账户')
+    cur.execute('create table if not exists customer \
+        (CID VARCHAR NOT NULL,\
+        CNAME VARCHAR NOT NULL,\
+        UID VARCHAR NOT NULL,\
+        ADDRES TEXT NOT NULL,\
+        PHONE VARCHAR\
+        );\
+    ')
 except:
     logger.warning('database connect failed')
     logger.warning(traceback.format_exc())
+finally:
+    conn.close()
 
 
 # cur.execute('''
