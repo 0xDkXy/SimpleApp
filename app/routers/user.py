095968e6f2cecbb59dcc6dfe155971c69881a603
0xDkXy <bo001015@outlook.com>
2022-01-04 06:43:00 +0800

    update:获取用户信息二维码

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,5 +1,7 @@
 from typing import Optional
 from fastapi import APIRouter
+from qrcode import image
+from qrcode.image import base
 from ..tools.Logger import logger 
 from pydantic import BaseModel
 from app.tools import token
@@ -9,6 +11,9 @@ from ..DataBase.customerDB import query,insert
 from ..core.aliveList import *
 from ..core.aliveList import aliveVerify
 from traceback import format_exc
+import qrcode
+from fastapi.responses import FileResponse
+import base64
 
 
 class customer(BaseModel):
@@ -71,5 +76,37 @@ def getCostInfo(input:userVerify):
     else:
         return {'msg':'wrong token'}
 
+@router.post('/getCostInfoAll')
+def getCostInfoAll(input:userVerify):
+    token=input.token
+    UID=input.UID
+    if token == None or UID == None :
+        return {'msg':'permission denied'}
+    if aliveVerify(UID,token):
+        try:
+            List=query(UID)
+        except Exception as e:
+            raise e
+        ret={}
+        for p,info in enumerate(List):
+            ret.update({
+                str(p):{
+                    'CID':info[0],
+                    'CName':info[1],
+                    'addres':info[2],
+                    'phone':info[3],
+                }
+            })
+        ret=str(ret).encode()
+        ret=base64.b64encode(ret).decode()
+        logger.info("UID:{} | ret:{}".format(UID,ret))
+        img=qrcode.make(ret)
+        imgPath='./data/{}_qrcode.png'.format(UID)
+        with open(imgPath,'wb') as f:
+            img.save(f)
+        return FileResponse(path=imgPath,filename=imgPath.split('/')[2])
+        # return 
+    else:
+        return {'msg':'wrong token'}
             
 
